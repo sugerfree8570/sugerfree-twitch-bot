1fe1bc9b37983049005fcd2e774b0e68b61c9373
BuildTools <unconfigured@null.spigotmc.org>
2021-07-18 00:50:59 +0800

    init commmit

---
+++ server.js
@@ -0,0 +1,56 @@
+/****
+ * tmijs
+ * https://github.com/tmijs/tmi.js
+ * 
+ * doc
+ * https://github.com/tmijs/docs/tree/gh-pages/_posts
+ * 
+ * follow by
+ * https://spacejelly.dev/posts/how-to-create-a-twitch-chat-bot-with-node-js-tmi-js-heroku/
+ *  
+ */
+
+
+
+require('dotenv').config();
+const tmi = require('tmi.js')
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true
+    },
+    channels: [
+        'sugerfree8570'
+    ],
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_ACCESS_TOKEN
+      }
+});
+
+client.connect();
+
+client.on('message', async (channel, context, message, self) => {
+
+    // ignore self message, prevent INF loop
+    if (self) return;
+
+    // client.say(channel, `hi! @${context.username} 晚上好RR `);
+
+    // ignore not command message (start with !)
+    if (!message.startsWith('!')) return;
+
+    // parse arg and commands
+    const args = message.slice(1).split(' ');
+	const cmd = args.shift().toLowerCase();
+
+    
+    if (cmd === 'echo' ) {
+        // response to chat
+        client.say(channel, `@${context.username}, you said: "${args.join(' ')}"`);
+    }
+
+});
+
+
+
